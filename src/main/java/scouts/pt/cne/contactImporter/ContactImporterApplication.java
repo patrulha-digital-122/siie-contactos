154a12f1ac6c7509e119f2929fdcbe66e3f29f65
André Conrado <andre.conrado@gls-itservices.com>
2017-11-16 18:05:46 +0000

    First Commit

---
+++ src/main/java/scouts/pt/cne/contactImporter/ContactImporterApplication.java
@@ -0,0 +1,12 @@
+package scouts.pt.cne.contactImporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactImporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactImporterApplication.class, args);
+	}
+}

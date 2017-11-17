8a499f6d568e1c7890e6b676e328864de6a7b40a
André Conrado <andre.conrado@gls-itservices.com>
2017-11-17 11:46:12 +0000

    Test vaadin with spring boot

---
+++ src/main/java/scouts/cne/pt/Application.java
@@ -0,0 +1,18 @@
+package scouts.cne.pt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author anco62000465 2017-11-17
+ *
+ */
+@SpringBootApplication
+public class Application
+{
+	
+	public static void main( String[] args )
+	{
+		SpringApplication.run(Application.class);
+	}
+}
